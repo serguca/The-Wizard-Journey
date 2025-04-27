eb6d85c2c28d66b83b44b2fb0485c5cb7c365ca9
serguca <serguca2003@digitechfp.com>
2025-04-27 19:00:43 +0200

    Creacion script que hereda de EnemyAI (recordatorio: verse un video sobre la herencia)

---
+++ Assets/Scripts/EnemyBlueMage.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class EnemyBlueMage : EnemyAi
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
